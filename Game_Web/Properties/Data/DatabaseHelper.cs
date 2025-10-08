9624790a053f2ef0a683b758352f877e027ec574
fleskens99 <franciscopdelf@gmail.com>
2025-10-08 09:13:37 +0200

    Add project files.

---
+++ Game_Web/Properties/Data/DatabaseHelper.cs
@@ -0,0 +1,51 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+
+public class DatabaseHelper
+{
+    private string connectionString = "Server=localhost;Database=database;Uid=root;Pwd=root;";
+
+    public void TestConnection()
+    {
+        using (MySqlConnection conn = new MySqlConnection(connectionString))
+        {
+            try
+            {
+                conn.Open();
+                Console.WriteLine("Connection successful!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Connection failed: " + ex.Message);
+            }
+        }
+    }
+
+    public List<Game> GetGames(string query)
+    {
+        DataTable dt = new DataTable();
+        using (MySqlConnection conn = new MySqlConnection(connectionString))
+        {
+            try
+            {
+                conn.Open();
+            }
+            catch (MySqlException exception)
+            {
+                throw new Exception("Database was not connected.", exception);
+            }
+
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            MySqlDataReader mySqlDataReader = new(cmd);
+            using (MySqlDataReader reader = mySqlDataReader)
+            {
+                while (reader.Read())
+                {
+
+                }
+            }
+        }
+        return dt;
+    }
+}
